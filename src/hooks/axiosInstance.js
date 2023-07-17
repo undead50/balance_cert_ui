8c08c003517e67835705d7edffdf249522a7e144
ayush pradhan <ayushpradhan588@gmail.com>
2023-07-17 21:46:26 +0545

    token added

--- src/hooks/axiosInstance.js
+++ src/hooks/axiosInstance.js
@@ -2,15 +2,13 @@
 
 import axios from 'axios';
 import { NavLink, Navigate } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { setUser, FlushUserData } from '../store/index';
+import { notification } from 'antd';
 
-const history = createBrowserHistory();
-
-
-let store
-export const injectStore = _store => {
-  store = _store
-}
+let store;
+export const injectStore = (_store) => {
+  store = _store;
+};
 
 const axiosInstance = axios.create({
   // Replace with your API base URL
@@ -18,57 +16,59 @@ const axiosInstance = axios.create({
 
 // Request interceptor
 axiosInstance.interceptors.request.use(
-  config => {
+  (config) => {
     // Modify the request config before sending the request
     // For example, you can add headers or authentication tokens
+    config.headers['Authorization'] = `Bearer ${
+      store.getState().user.userInfo.token
+    }`;
+    config.headers['Content-Type'] = 'application/json';
     console.log('Request Interceptor:', config);
-    // window.history.pushState(null, null,'/app')
-    
-    // alert(store.getState().user.userInfo.token)
-    // return config;
-    
-    
-    return config
+    return config;
   },
-  error => {
+  (error) => {
     // Handle request error
     console.error('Request Interceptor Error:', error);
     return Promise.reject(error);
   }
 );
 
+const erroHandler = (description, type) => {
+  const payload = {
+    userName: null,
+    solId: null,
+    email: null,
+    departmentName: null,
+    token: null,
+  };
+  notification.open({
+    message: 'info',
+    description: description,
+    duration: 3, // Duration in seconds, 0 means the notification won't close automatically,
+    type: type,
+  });
+  store.dispatch(setUser(payload));
+  store.dispatch(FlushUserData());
+};
+
 // Response interceptor
 axiosInstance.interceptors.response.use(
-  response => {
+  (response) => {
     // Modify the response data before passing it to the .then() callback
     console.log('Response Interceptor:', response);
     if (response && response.status === 200) {
-      // Handle the 404 error by redirecting to the specified URL
-      // window.history.pushState(null, null,'/login')
-
-      // userInfo.userName === null
-      // alert(store.getState().user.userInfo.token)
-      const action = { type: 'user/setUser', payload: { 
-        userName: null,
-        solId:null,
-        email:null,
-        departmentName:null,
-        token:null
-        } };
-      store.dispatch(action);      
-    } 
-
-      
       return response;
-    
+    }
   },
-  error => {
+  (error) => {
     // Handle response error
     console.error('Response Interceptor Error:', error);
 
     if (error.response && error.response.status === 404) {
       // Show an alert for a 404 error
-      alert('404 Error: Resource not found');
+      erroHandler(error.response, 'error');
+    } else if (error.response && error.response.status === 401) {
+      erroHandler('Token Expired Please login', 'error');
     }
     return Promise.reject(error);
   }
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,103 +1,114 @@
 import React, { useEffect } from 'react';
-import { Form, Input, Button ,Card } from 'antd';
+import { Form, Input, Button, Card } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './index.css';
 import Spinner from '../../components/Spinner';
 import { setUser } from '../../store';
-import {useSelector,useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
 import { postLoginData } from '../../store/slices/authSlice';
-import {useNotification} from '../../hooks/index'
+import { useNotification } from '../../hooks/index';
 
 const LoginPage = () => {
-    // alert('login')
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  // alert('login')
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const {callNotification} = useNotification();
+  const { callNotification } = useNotification();
 
-    const { data, loading, error } = useSelector((state) => state.auth);
-    // useNotification('Login Denied','error')
+  const { data, loading, error } = useSelector((state) => state.auth);
+  // useNotification('Login Denied','error')
 
-    useEffect(()=>{
-      if(data){
-        if (data.Code === "0"){
-          dispatch(setUser({
+  useEffect(() => {
+    if (data) {
+      if (data.Code === '0') {
+        dispatch(
+          setUser({
             userName: data.Data.domainUserName,
-            solId:data.Data.solId,
-            email:data.Data.email,
-            departmentName:data.Data.departmentName,
-            token:data.Data.token
-        }))
+            solId: data.Data.solId,
+            email: data.Data.email,
+            departmentName: data.Data.departmentName,
+            token: data.Data.token,
+          })
+        );
         navigate('/');
-        callNotification('Login Success','success')
+        callNotification('Login Success', 'success');
       } else {
-        callNotification('Login Denied','error')
+        callNotification('Login Denied', 'error');
       }
-      }
-    },[data])
+    }
+  }, [data]);
 
-    
-    const onFinish = (values) => {
+  const onFinish = (values) => {
     // Call the postData function from the custom hook
     const reqData = {
-      username:values.username,
-      password:values.password
-    }
-    dispatch(postLoginData(reqData))
+      username: values.username,
+      password: values.password,
+    };
+    dispatch(postLoginData(reqData));
     // alert(data)
-    console.log(data)
-    
-
+    console.log(data);
   };
 
   return (
-    <div style={{ minHeight: '100vh', background: 'linear-gradient(to right, #468CC1, #3EAB94)', display: 'flex',  alignItems:'center', justifyContent:'center' }}>
-    <Card style={{ maxWidth: '25%', margin: '0 auto'}}>
+    <div
+      style={{
+        minHeight: '100vh',
+        background: 'linear-gradient(to right, #468CC1, #3EAB94)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Card style={{ maxWidth: '25%', margin: '0 auto' }}>
+        <div style={{ textAlign: 'center', marginBottom: 24 }}>
+          <img
+            src={process.env.PUBLIC_URL + '/images/citizens-logo.png'}
+            alt="Logo"
+            style={{
+              height: 80,
+              width: '90%',
+              marginLeft: '20px',
+              maxHeight: '10vh',
+            }}
+          />
+        </div>
+        <u>
+          <h3 style={{ fontFamily: 'cursive' }}>Risk Assessment tool </h3>
+        </u>
 
-      <div style={{ textAlign: 'center', marginBottom: 24 }}> 
-        <img
-          src= {process.env.PUBLIC_URL + '/images/citizens-logo.png'}
-          alt="Logo"
-          style={{ height: 80, width: '90%',marginLeft:'20px',maxHeight:'10vh' }}
-        />
-      </div>
-      <u>
-        <h3 style={{fontFamily:'cursive'}}>Risk Assessment tool </h3>
-      </u>
-
-      <Form name="login-form" onFinish={onFinish}>
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: 'Please enter your username!',
-            },
-          ]}
-        >
-          <Input prefix={<UserOutlined />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please enter your password!',
-            },
-          ]}
-        >
-          <Input.Password prefix={<LockOutlined />} placeholder="Password" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
-            Log In
-          </Button>
-        </Form.Item>
-        {loading && <Spinner />}
-        {/* <Outlet/> */}
-      </Form>
-    </Card>
+        <Form name="login-form" onFinish={onFinish}>
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your password!',
+              },
+            ]}
+          >
+            <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+              Log In
+            </Button>
+          </Form.Item>
+          {loading && <Spinner />}
+          {/* <Outlet/> */}
+        </Form>
+      </Card>
     </div>
   );
 };
